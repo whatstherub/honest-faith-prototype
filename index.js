d72dedc451133aa15bd512ae213f01d36050fecf
paul <paulwilsonbrown@gmail.com>
2015-10-30 11:11:06 -1000

    Update basic auth

--- index.js
+++ index.js
@@ -8,11 +8,25 @@ var port = process.env['PORT'] || 8000,
     environment = process.env['NODE_ENV'] == 'production' ? 'production' : 'development';
 
 if( environment == 'production' ) {
+  app.use(function *(next){
+    try {
+      yield next;
+    } catch (err) {
+      if (401 == err.status) {
+        this.status = 401;
+        this.set('WWW-Authenticate', 'Basic');
+        this.body = 'cant haz that';
+      } else {
+        throw err;
+      }
+    }
+  });
+
   app.use(auth({
     name: 'honest',
     pass: 'faith'
   }));
-} 
+}
 
 app.use(serve('web'));
 app.listen(port);
