4be144bad3b65bd9ceda72377ed3aba9714c299e
paul <paulwilsonbrown@gmail.com>
2015-11-16 11:30:33 -1000

    reformat

--- web/api/products/index.js
+++ web/api/products/index.js
@@ -1,14 +1,14 @@
 function *r(next) {
   var products = [
-    { id: 1  , sku: 'DIAPERS'     , name: 'Diapers' },
-    { id: 332, sku: 'LOTION-AP'   , name: 'Lotion - Apricot' },
-    { id: 288, sku: 'EYE-CREAM'   , name: 'Eye Cream' },
+    { id: 1  , sku: 'DIAPERS'       , name: 'Diapers' },
+    { id: 332, sku: 'LOTION-AP'     , name: 'Lotion - Apricot' },
+    { id: 288, sku: 'EYE-CREAM'     , name: 'Eye Cream' },
     { id: 328, sku: 'CONDITIONER-AP', name: 'Conditioner - Apricot' },
-    { id: 203, sku: 'DHA', name: 'Baby DHA' },
-    { id: 246, sku: 'TOOTHBRUSH', name: 'Adult Toothbrush' },
-    { id: 290, sku: 'REFRESH', name: 'Refresh Clean Gel' },
-    { id: 334, sku: 'BUBBLE-LV', name: 'Bubble Bath - Lavender' },
-    { id: 285, sku: 'KONJAC', name: 'Konjac Sponge' }
+    { id: 203, sku: 'DHA'           , name: 'Baby DHA' },
+    { id: 246, sku: 'TOOTHBRUSH'    , name: 'Adult Toothbrush' },
+    { id: 290, sku: 'REFRESH'       , name: 'Refresh Clean Gel' },
+    { id: 334, sku: 'BUBBLE-LV'     , name: 'Bubble Bath - Lavender' },
+    { id: 285, sku: 'KONJAC'        , name: 'Konjac Sponge' }
   ];
 
   this.body = products;
