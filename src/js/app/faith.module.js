cf486056605effac4b44cb9ccfb630c838e3bfc8
paul <paulwilsonbrown@gmail.com>
2015-11-16 09:24:32 -1000

    Checkpoint further reorg

--- src/js/app/faith.module.js
+++ src/js/app/faith.module.js
@@ -1,19 +1,10 @@
 import inventoryModule from 'common.components.inventory';
-import inventoryServicesModule from 'common.services.inventory';
-import productProjectionModule from 'common.components.inventory.product_projection';
-import productProjectionSummaryModule from 'common.components.inventory.product_projection_summary';
-import productProjectionDetailModule from 'common.components.inventory.product_projection_detail';
-import productProjectionTweaksModule from 'common.components.inventory.product_projection_tweaks';
-import atRiskProductsModule from 'common.components.inventory.at_risk_products';
 
 var moduleName = 'faith';
 
 angular.module(moduleName, [
   'ngAnimate', 'ngSanitize', 'templates', 'ui.router',
-  'highcharts-ng',
-  inventoryModule, inventoryServicesModule, productProjectionModule,
-  productProjectionSummaryModule, productProjectionDetailModule,
-  productProjectionTweaksModule, atRiskProductsModule
+  inventoryModule
 ]);
 
 export default moduleName;
--- src/js/common/components/inventory/inventory.module.js
+++ src/js/common/components/inventory/inventory.module.js
@@ -1,8 +1,20 @@
 import LoadingDirective from 'common.components.inventory.loading.directive';
 
+import inventoryServicesModule from 'common.services.inventory';
+import productProjectionModule from 'common.components.inventory.product_projection';
+import productProjectionSummaryModule from 'common.components.inventory.product_projection_summary';
+import productProjectionDetailModule from 'common.components.inventory.product_projection_detail';
+import productProjectionTweaksModule from 'common.components.inventory.product_projection_tweaks';
+import atRiskProductsModule from 'common.components.inventory.at_risk_products';
+
 var moduleName = 'common.components.inventory';
 
-angular.module(moduleName, ['ngAnimate','mgcrea.ngStrap', 'ui.select'])
+angular.module(moduleName, [
+  'ngAnimate','mgcrea.ngStrap', 'ui.select', 'highcharts-ng',
+  inventoryServicesModule, productProjectionModule,
+  productProjectionSummaryModule, productProjectionDetailModule,
+  productProjectionTweaksModule, atRiskProductsModule
+])
   .directive('loading', LoadingDirective.builder);
 
 export default moduleName;
